f17fccb669418cbf53b66ce15f51787c8b8297b0
elducati <geomondi09@gmail.com>
2019-09-27 15:46:10 +0300

    added update and delete

--- backend/app.js
+++ backend/app.js
@@ -13,68 +13,100 @@ mongoose.connect('mongodb+srv://jeff007:tczJd3dArrDW3XGM@cluster0-shtxy.mongodb.
     console.error(error);
   });
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 app.use(bodyParser.json());
 
 app.post('/api/recipes', (req, res, next) => {
-    const recipe = new Recipe({
-        recipeName: req.body.recipeName,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        userId: req.body.userId,
-        prepTime: req.body.prepTime,
-        cookTime: req.body.cookTime,
-        recipeIngredients: req.body.recipeIngredients,
-        recipeInstructions: req.body.recipeInstructions
-    });
-    recipe.save().then(
-      () => {
-        res.status(201).json({
-          message: 'Post saved successfully!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
+  const recipe = new Recipe({
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    time: req.body.time,
+    difficulty: req.body.difficulty,
   });
-app.use((req, res) => {
-    res.json({ message: 'Your request was successful!' });
+  recipe.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+app.put('/api/recipes/:id', (req, res, next) => {
+  const recipe = new Recipe({
+    _id: req.params.id,
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    time: req.body.time,
+    difficulty: req.body.difficulty,
+  });
+  Recipe.updateOne({ _id: req.params.id }, recipe).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
 });
 app.get('/api/recipes/:id', (req, res, next) => {
-    Recipe.findOne({
-      _id: req.params.id
-    }).then(
-      (recipe) => {
-        res.status(200).json(recipe);
-      }
-    ).catch(
-      (error) => {
-        res.status(404).json({
-          error: error
-        });
-      }
-    );
-  });
+  Recipe.findOne({
+    _id: req.params.id
+  }).then(
+    (recipe) => {
+      res.status(200).json(recipe);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
+app.delete('/api/recipes/:id', (req, res, next) => {
+  Recipe.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
 app.use('/api/recipes', (req, res, next) => {
-    Recipe.find().then(
-      (recipe) => {
-        res.status(200).json(recipes);
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-  
+  Recipe.find().then(
+    (recipes) => {
+      res.status(200).json(recipes);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
 module.exports = app;
--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = mongoose.Schema({
-  recipeName: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  userId: { type: String, required: true },
-  prepTime:{type: String, required: true },
-  cookTime: { type: String, required: true },
-  recipeIngredients: { type: String, required: true },
-  recipeInstructions: { type: String, required: true },
+  
+  title: { type: String, required: true },
+  ingredients: { type: String, required: true },
+  instructions: { type: String, required: true },  
+  time:{ type: Number, required: true },
+  difficulty: { type: Number, required: true }  
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
